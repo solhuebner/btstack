8eff314a1d6eb8ecf85c8aceb178c2e5d7e38d4b
Simon Budig <simon@budig.de>
2022-06-09 13:42:01 +0200

    opp_client: implement pulling the default object, handle the "not implemented" response from my phone.

--- src/classic/obex.h
+++ src/classic/obex.h
@@ -57,6 +57,7 @@
 #define OBEX_RESP_FORBIDDEN                0xC3
 #define OBEX_RESP_NOT_FOUND                0xC4
 #define OBEX_RESP_NOT_ACCEPTABLE           0xC6
+#define OBEX_RESP_NOT_IMPLEMENTED          0xD1
 
 #define OBEX_HEADER_BODY                           0x48
 #define OBEX_HEADER_END_OF_BODY                    0x49
